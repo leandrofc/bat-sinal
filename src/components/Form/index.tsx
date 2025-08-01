92a7741fdb09b59494abd3510919b0068eb7afa0
Leandro Fonseca <leandromkt@outlook.com>
2025-08-01 11:10:26 -0300

    feat: scroll to input focus

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,4 +1,4 @@
-import { TextInput, TouchableWithoutFeedback, View, Keyboard } from "react-native"
+import { TextInput, TouchableWithoutFeedback, View, Keyboard, KeyboardAvoidingView, Platform, ScrollView } from "react-native"
 import { BatLogo } from "../BatLogo"
 import { TextInputComponent } from "../TextInputComponent"
 import Button from "../Button"
@@ -22,62 +22,85 @@ const Form = ({ sendFunction }: FormProps) => {
         sendFunction();
     };
 
+    const scrollViewRef = useRef<ScrollView>(null);
+    const nameRef = useRef<TextInput>(null);
     const phoneRef = useRef<TextInput>(null);
     const locationRef = useRef<TextInput>(null);
     const observationRef = useRef<TextInput>(null);
 
+    const onFocusInput = (inputRef: React.RefObject<TextInput | null>) => {
+        setTimeout(() => {
+          inputRef.current?.measure((x, y, width, height, pageX, pageY) => {
+            scrollViewRef.current?.scrollTo({ y: pageY - 100, animated: true });
+          });
+        }, 100);
+    };
+      
     return (
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View style={styles.container}>
-                <View>
-                    <BatLogo isSmall />
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'} // diferente para iOS e Android
+            keyboardVerticalOffset={Platform.OS === 'ios' ? 60 : 0} // ajuste de distância
+        >
+            <ScrollView keyboardShouldPersistTaps="handled" ref={scrollViewRef}>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View style={styles.container}>
+                    <View>
+                        <BatLogo isSmall />
+                    </View>
+                    <View style={styles.inputsContainer}>
+                        <TextInputComponent 
+                            label="Name"
+                            onChangeText={setName}
+                            placeholder="Type Your name"
+                            value={name}
+                            ref={nameRef}
+                            returnKeyType="next"
+                            onSubmitEditing={() => phoneRef.current?.focus()}
+                            onFocus={() => onFocusInput(nameRef)}
+                        />
+                        <TextInputComponent 
+                            keyboardType="phone-pad"
+                            label="Phone"
+                            onChangeText={setPhone}
+                            placeholder="Type Your phone"
+                            value={phone}
+                            ref={phoneRef}
+                            returnKeyType="next"
+                            onSubmitEditing={() => locationRef.current?.focus()}
+                            onFocus={() => onFocusInput(phoneRef)}
+                        />
+                        <TextInputComponent 
+                            label="Location"
+                            onChangeText={setLocation}
+                            placeholder="Type Your location"
+                            value={location}
+                            ref={locationRef}
+                            returnKeyType="next"
+                            onSubmitEditing={() => observationRef.current?.focus()}
+                            onFocus={() => onFocusInput(locationRef)}
+                        />
+                        <TextInputComponent 
+                            label="Observation"
+                            onChangeText={setObservation}
+                            placeholder="Type Your observation"
+                            value={observation}
+                            isTextArea
+                            ref={observationRef}
+                            returnKeyType="done"
+                            onSubmitEditing={handleSubmit}
+                            blurOnSubmit={true}
+                            onFocus={() => onFocusInput(observationRef)}
+                        />
+                        <Button
+                            text="Send"
+                            onPress={handleSubmit}
+                        />
+                    </View>
                 </View>
-                <View style={styles.inputsContainer}>
-                    <TextInputComponent 
-                        label="Name"
-                        onChangeText={setName}
-                        placeholder="Type Your name"
-                        value={name}
-                        returnKeyType="next"
-                        onSubmitEditing={() => phoneRef.current?.focus()}
-                    />
-                    <TextInputComponent 
-                        keyboardType="phone-pad"
-                        label="Phone"
-                        onChangeText={setPhone}
-                        placeholder="Type Your phone"
-                        value={phone}
-                        ref={phoneRef}
-                        returnKeyType="next"
-                        onSubmitEditing={() => locationRef.current?.focus()}
-                    />
-                    <TextInputComponent 
-                        label="Location"
-                        onChangeText={setLocation}
-                        placeholder="Type Your location"
-                        value={location}
-                        ref={locationRef}
-                        returnKeyType="next"
-                        onSubmitEditing={() => observationRef.current?.focus()}
-                    />
-                    <TextInputComponent 
-                        label="Observation"
-                        onChangeText={setObservation}
-                        placeholder="Type Your observation"
-                        value={observation}
-                        isTextArea
-                        ref={observationRef}
-                        returnKeyType="done"
-                        onSubmitEditing={handleSubmit}
-                        blurOnSubmit={true}
-                    />
-                    <Button
-                        text="Send"
-                        onPress={handleSubmit}
-                    />
-                </View>
-            </View>
-        </TouchableWithoutFeedback>
+            </TouchableWithoutFeedback>
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
 
