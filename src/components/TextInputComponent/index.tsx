c42033e3f5782de4cb2ea9161ae74691989e598c
Leandro Fonseca <leandromkt@outlook.com>
2025-08-01 11:10:08 -0300

    fix: adjust TextInputComponent to received ref in a correct way

--- src/components/TextInputComponent/index.tsx
+++ src/components/TextInputComponent/index.tsx
@@ -1,53 +1,61 @@
-import React, { useState } from 'react';
-import {TextInput, View, Text } from 'react-native';
+import React, { useState, forwardRef } from 'react';
+import { TextInput, View, Text } from 'react-native';
 import { styles } from './index.styles';
-import { TextInputComponentProps } from './index.types';
+import type { TextInputComponentProps } from './index.types';
 
-  
-export function TextInputComponent({
-  label,
-  onChangeText,
-  placeholder,
-  value,
-  onSubmitEditing,
-  returnKeyType,
-  ref,
-  isTextArea = false,
-  blurOnSubmit,
-  keyboardType,
-} : TextInputComponentProps) {
-  const [isFocused, setIsFocused] = useState(false);
+export const TextInputComponent = forwardRef<TextInput, TextInputComponentProps>(
+  (
+    {
+      label,
+      onChangeText,
+      placeholder,
+      value,
+      onSubmitEditing,
+      returnKeyType,
+      isTextArea = false,
+      blurOnSubmit,
+      keyboardType,
+      onFocus,
+    },
+    ref
+  ) => {
+    const [isFocused, setIsFocused] = useState(false);
 
-  return (
-    <View style={styles.container}>
-      <Text
-        style={[
-          styles.label,
-          isFocused && styles.labelForInputFocus
-        ]}>
-        {label}
-      </Text>
-      <View style={styles.inputContainer}>
-        <TextInput
+    return (
+      <View style={styles.container}>
+        <Text
           style={[
-            styles.input,
-            isTextArea && styles.inputTextArea,
-            isFocused && styles.inputFocus,
+            styles.label,
+            isFocused && styles.labelForInputFocus,
           ]}
-          placeholder={placeholder}
-          value={value}
-          onChangeText={onChangeText}
-          placeholderTextColor="#676767"
-          multiline={isTextArea}
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-          onSubmitEditing={onSubmitEditing}
-          returnKeyType={returnKeyType}
-          ref={ref}
-          blurOnSubmit={blurOnSubmit}
-          keyboardType={keyboardType}
-        />
+        >
+          {label}
+        </Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={[
+              styles.input,
+              isTextArea && styles.inputTextArea,
+              isFocused && styles.inputFocus,
+            ]}
+            placeholder={placeholder}
+            value={value}
+            onChangeText={onChangeText}
+            placeholderTextColor="#676767"
+            multiline={isTextArea}
+            onFocus={(e) => {
+              setIsFocused(true);
+              onFocus?.(e);
+            }}
+            onBlur={() => setIsFocused(false)}
+            onSubmitEditing={onSubmitEditing}
+            returnKeyType={returnKeyType}
+            ref={ref}
+            blurOnSubmit={blurOnSubmit}
+            keyboardType={keyboardType}
+          />
+        </View>
       </View>
-    </View>
-  );
-}
+    );
+  }
+);
--- src/components/TextInputComponent/index.types.ts
+++ src/components/TextInputComponent/index.types.ts
@@ -1,26 +1,34 @@
-import { RefObject } from "react";
-import type { NativeSyntheticEvent, TextInput, TextInputSubmitEditingEventData } from "react-native";
-
-export interface TextInputComponentProps {
-    label: string,
-    placeholder: string,
-    value: string,
-    onChangeText: (text: string) => void,
-    isTextArea?: boolean,
-    returnKeyType?: 'done' | 'go' | 'next' | 'search' | 'send' | 'none' | 'previous' | 'default' | 'emergency-call' | 'google' | 'join' | 'route' | 'yahoo';
+import type {
+    NativeSyntheticEvent,
+    TextInputFocusEventData,
+    TextInputSubmitEditingEventData,
+  } from "react-native";
+  
+  export interface TextInputComponentProps {
+    label: string;
+    placeholder: string;
+    value: string;
+    onChangeText: (text: string) => void;
+    isTextArea?: boolean;
+    returnKeyType?:
+      | 'done' | 'go' | 'next' | 'search' | 'send'
+      | 'none' | 'previous' | 'default'
+      | 'emergency-call' | 'google' | 'join'
+      | 'route' | 'yahoo';
     onSubmitEditing?: (e: NativeSyntheticEvent<TextInputSubmitEditingEventData>) => void;
-    ref?: RefObject<TextInput | null>;
-    blurOnSubmit?: boolean,
-    keyboardType?: 
-        | 'default'
-        | 'email-address'
-        | 'numeric'
-        | 'phone-pad'
-        | 'number-pad'
-        | 'decimal-pad'
-        | 'visible-password'
-        | 'url'
-        | 'name-phone-pad'
-        | 'twitter'
-        | 'web-search';
-}
+    blurOnSubmit?: boolean;
+    keyboardType?:
+      | 'default'
+      | 'email-address'
+      | 'numeric'
+      | 'phone-pad'
+      | 'number-pad'
+      | 'decimal-pad'
+      | 'visible-password'
+      | 'url'
+      | 'name-phone-pad'
+      | 'twitter'
+      | 'web-search';
+    onFocus?: (e: NativeSyntheticEvent<TextInputFocusEventData>) => void;
+  }
+  
