c00e79940f579089ccff698810bb5b274016bdd6
Leandro Fonseca <leandromkt@outlook.com>
2025-07-31 22:51:25 -0300

    feat:show TextInputComponent on the App.tsx

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Button from './src/components/Button';
 import { BatLogo } from './src/components/BatLogo';
+import { TextInputComponent } from './src/components/TextInputComponent';
 
 export default function App() {
   return (
@@ -9,6 +10,15 @@ export default function App() {
       <StatusBar style="light" />
       <View>
         <BatLogo/>
+      </View>
+      <View>
+        <TextInputComponent
+          label='Name'
+          value=""
+          onChange={() => {}}
+          placeholder='Type your name'
+          isTextArea
+        />
       </View>
       <Button text="ACTIVATED BAT SINAL" onPress={() => {console.log("oi")}} />
     </View>
@@ -21,6 +31,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#101010',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 24
+    padding: 24,
   },
 });
