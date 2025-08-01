e22bf57b108927b78ad7eaf9e55a9d03c3210628
Leandro Fonseca <leandromkt@outlook.com>
2025-07-31 22:58:37 -0300

    feat: adjust BatLogo to show smallImage when isSmall prop is received

--- src/components/BatLogo/index.tsx
+++ src/components/BatLogo/index.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 
 import batLogo from '../../../assets/batLogo.png';
+import batLogoSmall from '../../../assets/batLogoSmall.png';
+import { BatLogoProps } from './index.types';
+import { styles } from './index.styles';
 
-export function BatLogo() {
+export function BatLogo({ isSmall = false }: BatLogoProps) {
   return (
-    <Image source={batLogo} />
+    <Image
+      source={isSmall ? batLogoSmall : batLogo}
+      style={isSmall && styles.smallImage}
+    />
   );
 }
